4bb0e635c86219ee4d23246da38f0173716ed683
dawidbera <10242059+dawidbera@users.noreply.github.com>
2026-02-26 21:54:04 +0000

    Initial commit in market-data-pipeline

---
+++ ingestor-service/src/main/java/com/market/pipeline/ingestor/IngestorApplication.java
@@ -0,0 +1,11 @@
+package com.market.pipeline.ingestor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IngestorApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(IngestorApplication.class, args);
+    }
+}
---
+++ processor-service/src/main/java/com/market/pipeline/processor/ProcessorApplication.java
@@ -0,0 +1,13 @@
+package com.market.pipeline.processor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.EnableKafkaStreams;
+
+@SpringBootApplication
+@EnableKafkaStreams
+public class ProcessorApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ProcessorApplication.class, args);
+    }
+}
